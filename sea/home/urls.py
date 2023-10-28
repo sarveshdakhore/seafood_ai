d1d03c6d1927ea802a7b8352acf71d2119a4c700
sarveshdakhore <sarveshd444@gmail.com>
2023-10-29 00:29:53 +0530

    nav bar done

--- sea/home/urls.py
+++ sea/home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
--- sea/home/views.py
+++ sea/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-#create a view for home page of the website with some context paased with it
+#create a view for home page of the website with some context paased with it and the html file it should load is in the home folder templates/home/home.html
 def home(request):
     context = {'home': 'active'}
     return render(request, 'home/home.html', context)
