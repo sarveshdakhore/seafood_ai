9caa831858be9cbdef26799edfdd0908c43f4ca4
Harsh Shah <71080727+SamShah30112004@users.noreply.github.com>
2023-10-29 06:08:25 +0530

    Update translate_text.py to auto detect language

--- translate_text.py
+++ translate_text.py
@@ -2,10 +2,12 @@ from googletrans import Translator
 
 translator = Translator()
 
-def translate_to_en(lang, text):
+def translate_to_en(text):
+    lang = translator.detect(text).lang
     translated = translator.translate(text, src=lang, dest='en')
     return translated.text
 
-def translate_to_lang(lang, text):
+def translate_to_lang(text):
+    lang = translator.detect(text).lang
     translated = translator.translate(text, src='en', dest=lang)
     return translated.text
