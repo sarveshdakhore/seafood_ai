040dd9594fc8370d3925c1db4fd1a184d77e540a
sarveshdakhore <sarveshd444@gmail.com>
2023-10-29 11:41:37 +0530

    final1

--- sea/home/views.py
+++ sea/home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 #create a view for home page of the website with some context paased with it and the html file it should load is in the home folder templates/home/home.html
 def home(request):
-    context = {'home': 'active'}
+    context = {'home': 'active','chat':'True'}
     return render(request, 'home/home.html', context)
